8ec8ba55f4d776643f0f5d5eaeefea609755037b
Ledka17 <ledkaconf@gmail.com>
2019-04-08 18:56:30 +0300

    added Mail_Class testind cpp

--- CatchTest_Mail_Class.cpp
+++ CatchTest_Mail_Class.cpp
@@ -8,5 +8,18 @@ TEST_CASE( "Mail class checker", "[Mail.save_file]" ) {
     REQUIRE( mail.save_file("", 12) == false );
     REQUIRE( mail.save_file("resumes", 1) == true );
     REQUIRE( mail.save_file("Project/HR", 444) == true );
-    REQUIRE( mail.save_file("", 12) == false );
+    REQUIRE( mail.save_file("", 0) == false );
+    REQUIRE( mail.save_file("file", -57) == false );
+    REQUIRE( mail.save_file("file", 0) == false );
+}
+
+TEST_CASE( "Mail class checker", "[Mail.connect]" ) {
+    Mail mail;
+    REQUIRE( mail.connect("login", "") == false );
+    REQUIRE( mail.connect("login@gmail.com", "qwerty") == true );
+    REQUIRE( mail.connect("ProjectHR@yandex.ru", "super") == true );
+    REQUIRE( mail.connect("", "") == false );
+    REQUIRE( mail.connect("pavel@mail.ru", "") == false );
+    REQUIRE( mail.connect("", "password") == false );
+    REQUIRE( mail.connect("///", "true") == false );
 }
